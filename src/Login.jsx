34fc3f245a98238371a4edb0bd0dc9c164135c94
Harshit Kulkarni <kharshitr@gmail.com>
2025-03-19 09:10:57 +0530

    changesin routing

--- src/Login.jsx
+++ src/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
       //console.log(res.data);
       dispatch(addUser(res.data));
-      return navigate("/");
+      return navigate("/feed");
     } catch (error) {
       //console.log(error);
       setError(error?.response?.data);
